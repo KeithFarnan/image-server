fa5b5ca1a1b0c20420c64ba609e0c8f8a2e616bc
m <m@localhost.localdomain>
2019-06-10 16:34:59 +0100

    Added route to the Image.js file

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
-const express = require('express');
+const express = require("express");
 
 // executes express like a function allowing us to use methods
 const app = express();
 
-const productRoutes = require('./Api/Routes/Products');
-const orderRoutes = require('./Api/Routes/Orders');
-const imageRoutes = require('./Api/Routes/Images');
+// creating the const values which referenc the API routes file locations for each element
+const productRoutes = require("./Api/Routes/Products");
+const orderRoutes = require("./Api/Routes/Orders");
+const imageRoutes = require("./Api/Routes/Images");
 
-// all urls with /products will use productRoutes files
-// this way allows us to split up the code into files
-app.use('/products', productRoutes);
-app.use('/orders', orderRoutes);
-app.use('/images', imageRoutes);
+// all urls with /images will use imageRoutes files
+// this way allows us to split up the code into seperate files
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
+app.use("/images", imageRoutes);
 
 module.exports = app;
