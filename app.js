26aeb6917826e493eddf9ee45b94dc4cb2a10453
m <m@localhost.localdomain>
2019-06-10 19:00:43 +0100

    Added error message for wrong url

--- app.js
+++ app.js
@@ -8,10 +8,33 @@ const productRoutes = require("./api/router/products");
 const orderRoutes = require("./api/router/orders");
 const imageRoutes = require("./api/router/images");
 
+// all requests are funnelled through this middleware which logs the data and then lets it continue
+// morgan behind the scenes will use the next fuction saying "i logged it now you do something"
+const morgan = require("morgan");
+
+app.use(morgan("dev"));
+
 // all urls with /images will use imageRoutes files
 // this way allows us to split up the code into seperate files
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
 app.use("/images", imageRoutes);
+
+// all requests that are not any of the above are handelled below
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  //use specific error status else use 500 for generic
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const http = require("http");
-
 const app = require("./app");
 
 // use port issued or port 3000 if not assigned
 const port = process.env.PORT || 3000;
 
+// creates the server using the imported app object
 const server = http.createServer(app);
 
+// prompts the server to listen on the port set above
 server.listen(port);
