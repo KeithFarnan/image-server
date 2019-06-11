e585b72974d31bf87a5cbcf8da640e20ea2ec80d
keith <keith@keiths-air.wireless.nuigalway.ie>
2019-06-11 15:48:06 +0100

    created api methods for products

--- api/router/products.js
+++ api/router/products.js
@@ -1,50 +1,105 @@
 const express = require("express");
-
 const router = express.Router();
+const Product = require("../models/product");
+const mongoose = require("mongoose");
 
 router.get("/", (req, res, next) => {
-  res.status(200).json({
-    message: "This is a Json GET request :)"
-  });
+  Product.find()
+    .exec()
+    .then(docs => {
+      console.log(docs);
+      res.status(200).json(docs);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
 });
 
 router.post("/", (req, res, next) => {
   // Creating new product as javaScript Object
   // what is expected is stated in the documentation
-  const product = {
+  // Passing JavaScript objec as the parameters for the object
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
     price: req.body.price
-  };
-  res.status(201).json({
-    message: "Handelling POST request to /products",
-    createdProduct: product
   });
+  product
+    .save()
+    .then(result => {
+      console.log(result);
+      res.status(201).json({
+        message: "Handelling POST request to /products",
+        createdProduct: result
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
 });
 
 // : sets up the the valuesis a variable
 router.get("/:productId", (req, res, next) => {
   const id = req.params.productId;
-  if (id === "special") {
-    res.status(200).json({
-      message: " This is the special URL"
+  Product.findById(id)
+    .exec()
+    .then(doc => {
+      console.log("From the db", doc);
+      if (doc) {
+        res.status(200).json(doc);
+      } else {
+        res.status(404).json({
+          message: "No product found for this id"
+        });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
     });
-  } else {
-    res.status(200).json({
-      message: "You passed an id"
-    });
-  }
 });
 
 router.patch("/:productId", (req, res, next) => {
-  res.status(200).json({
-    message: "Updated Product"
-  });
+  const id = req.params.productId;
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+  Product.update({ _id: id }, { $set: updateOps })
+    .exec()
+    .then(result => {
+      console.log(result);
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
 });
 
 router.delete("/:productId", (req, res, next) => {
-  res.status(200).json({
-    message: "Product deleted"
-  });
+  const id = req.params.productId;
+  Product.remove({
+    _id: id
+  })
+    .exec()
+    .then(result => {
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
 });
 
 module.exports = router;
