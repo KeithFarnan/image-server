a5a9226f4c1de4e0e4ada12dfd7c2f7bb5b697b9
keith <keith@keiths-air.wireless.nuigalway.ie>
2019-06-11 12:32:58 +0100

    Set up error handelling

--- app.js
+++ app.js
@@ -22,15 +22,29 @@ app.use("/images", imageRoutes);
 
 // all requests that are not any of the above are handelled below
 app.use((req, res, next) => {
+  //  new const of Error object with the text "Not Found"
   const error = new Error("Not Found");
+  // setting the status code property
   error.status = 404;
+  // Executes the next method fowarding the error object
   next(error);
 });
 
+/*
+This method will handle all kinds of errors
+including database errors
+New First arguement 'error' 
+*/
 app.use((error, req, res, next) => {
-  //use specific error status else use 500 for generic
+  /*
+  use specific error status else use 500 for generic 
+  server error as response object 
+  */
   res.status(error.status || 500);
+  // return JSON object
   res.json({
+    // Error object which contains message property
+    // to which the error message which we passed in as first arguement
     error: {
       message: error.message
     }
