3927e5de195ed2f0c2c3ddb5ecb20c36827b2c9c
keith <keith@keiths-air.wireless.nuigalway.ie>
2019-06-11 12:46:10 +0100

    imported body parser

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const express = require("express");
-
 // executes express like a function allowing us to use methods
 const app = express();
+// all requests are funnelled through this middleware which logs the data and then lets it continue
+// morgan behind the scenes will use the next fuction saying "i logged it now you do something"
+const morgan = require("morgan");
+// importing the body parser method
+const bodyParser = require("body-parser");
 
 // creating the const values which referenc the API routes file locations for each element
 const productRoutes = require("./api/router/products");
 const orderRoutes = require("./api/router/orders");
 const imageRoutes = require("./api/router/images");
 
-// all requests are funnelled through this middleware which logs the data and then lets it continue
-// morgan behind the scenes will use the next fuction saying "i logged it now you do something"
-const morgan = require("morgan");
-
+// prompting the server to log the data befor it is passed to the routes
 app.use(morgan("dev"));
+
+/*
+Passing url encoded bodys
+extended false means only simple bodys will be parsed 
+*/
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// This will extract JSON data in an easily readable format
+app.use(bodyParser.json);
 
 // all urls with /images will use imageRoutes files
 // this way allows us to split up the code into seperate files
