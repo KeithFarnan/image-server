dfe579097f65252aea7e0f44f3a24ae6b355770c
keith <keith@keiths-air.wireless.nuigalway.ie>
2019-06-12 14:17:19 +0100

    Updated GET request

--- api/router/images.js
+++ api/router/images.js
@@ -1,9 +1,36 @@
 const express = require("express");
 const router = express.Router();
+const Image = require("../models/image");
+const mongoose = require("mongoose");
 
 router.get("/", (req, res, next) => {
-  image.find().se;
-  res.status(200).json({});
+  Image.find()
+    .select("name location date _id")
+    .exec()
+    .then(docs => {
+      const response = {
+        count: docs.length,
+        products: docs.map(doc => {
+          return {
+            name: doc.name,
+            location: doc.location,
+            date: doc.date,
+            _id: doc._id,
+            request: {
+              type: "GET",
+              url: "http://localhost:3000/images/" + doc._id
+            }
+          };
+        })
+      };
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
 });
 
 router.post("/", (req, res, next) => {
