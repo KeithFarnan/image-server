89a87c234266be0eecca8b5b3c2fa510f46d26e2
keith <keith@keiths-air.wireless.nuigalway.ie>
2019-06-13 11:51:23 +0100

    Created user Schema

---
+++ api/models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+// Pass a javaScript object to the schema method for how the product should look
+const userSchema = mongoose.Schema({
+  // _id is convention
+  //   ObjectId specific and  unique
+  _id: mongoose.Schema.Types.ObjectId,
+  name: { type: String, required: true },
+  password: { type: String, required: true }
+});
+// this has a capital letter by convention
+module.exports = mongoose.model("User", userSchema);
