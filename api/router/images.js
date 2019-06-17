b0a5f84fcf0e1f1a49dddb0d8c55025ef9ae0acd
keith <keith@keiths-air.wireless.nuigalway.ie>
2019-06-17 11:11:50 +0100

    Created and implemented multer methods

--- api/router/images.js
+++ api/router/images.js
@@ -2,12 +2,39 @@ const express = require("express");
 const router = express.Router();
 const Image = require("../models/image");
 const mongoose = require("mongoose");
+const multer = require("multer");
 const checkAuth = require("../middleware/check-auth");
 
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, "./uploads/");
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+const filefilter = (req, file, cb) => {
+  //reject a file == false save == true
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+// defining where and how the files are stored
+const upload = multer({
+  storage: storage,
+  limits: {
+    // limiting filesize to 8mb
+    fileSize: 1024 * 1024 * 8
+  },
+  fileFilter: filefilter
+});
 // GET request for all images
 router.get("/", (req, res, next) => {
   Image.find()
-    .select("_id date name location")
+    .select("_id date name location url")
     .exec()
     .then(docs => {
       const response = {
@@ -18,9 +45,10 @@ router.get("/", (req, res, next) => {
             date: doc.date,
             name: doc.name,
             location: doc.location,
+            url: doc.url,
             request: {
-              type: "GET",
-              url: "http://localhost:3000/images/" + doc._id
+              type: "GET"
+              //url: "http://localhost:3000/images/" + doc._id
             }
           };
         })
@@ -35,7 +63,8 @@ router.get("/", (req, res, next) => {
     });
 });
 // POST request path execute left to rigth so the auth will go befor the rest
-router.post("/", (req, res, next) => {
+router.post("/", upload.single("picture"), (req, res, next) => {
+  console.log(req.file);
   // Creating new product as javaScript Object
   // what is expected is stated in the documentation
   // Passing JavaScript objec as the parameters for the object
@@ -43,7 +72,8 @@ router.post("/", (req, res, next) => {
     _id: new mongoose.Types.ObjectId(),
     date: req.body.date,
     name: req.body.name,
-    location: req.body.location
+    location: req.body.location,
+    url: req.file.path
   });
   image
     .save()
@@ -55,9 +85,10 @@ router.post("/", (req, res, next) => {
           date: result.date,
           name: result.name,
           location: result.location,
+          url: result.url,
           request: {
-            type: "post",
-            url: "http://localhost:3000/images/" + result._id
+            type: "post"
+            //url: "http://localhost:3000/images/" + result._id
           }
         }
       });
@@ -74,7 +105,7 @@ router.post("/", (req, res, next) => {
 router.get("/:imageId", (req, res, next) => {
   const id = req.params.imageId;
   Image.findById(id)
-    .select("_id date name location")
+    .select("_id date name location url")
     .exec()
     .then(doc => {
       if (doc) {
