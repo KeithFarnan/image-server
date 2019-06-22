47a9b7b8217764c4d2100c7e0d5c3198bbb51ce2
m <m@localhost.localdomain>
2019-06-22 12:54:40 +0100

    changed the port the server runs on

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const app = require("./app");
 
 // use port issued or port 3000 if not assigned
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // creates the server using the imported app object
 const server = http.createServer(app);
