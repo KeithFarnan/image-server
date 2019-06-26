557a6f4daf8bd6cf6150fc097c9a09ad49fe3910
keith <keith@keiths-air.wireless.nuigalway.ie>
2019-06-26 16:03:07 +0100

    Removed unused files

--- reactWebApp/src/index.js
+++ reactWebApp/src/index.js
@@ -2,9 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import ImageFolders from "./Components/Pages/ImageFolders";
-import Upload from "./Components/Pages/ImageUpload/Upload";
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
