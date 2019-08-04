73ecef1fd9d36c9ffaf22539bcd36b82e606750c
Keith <k.farnan1@nuigalway.ie>
2019-08-04 21:16:05 +0100

    updated user.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,25 +1,26 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const gravatar = require('gravatar');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const { check, validationResult } = require('express-validator/check');
-const User = require('../../models/user');
-require('dotenv').config();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { check, validationResult } = require("express-validator/check");
+const User = require("../../models/user");
+require("dotenv").config();
 
+//! This allows the user to create a new profile on the website
 // @route    POST api/users
 // @desc     Register user
 // @access   Public
 router.post(
-  '/',
+  "/",
   [
-    check('name', 'Name is required')
+    check("name", "Name is required")
       .not()
       .isEmpty(),
-    check('email', 'Please include a valid email').isEmail(),
+    check("email", "Please include a valid email").isEmail(),
     check(
-      'password',
-      'Please enter a password with 6 or more characters'
+      "password",
+      "Please enter a password with 6 or more characters"
     ).isLength({ min: 6 })
   ],
   async (req, res) => {
@@ -33,12 +34,12 @@ router.post(
       if (user) {
         return res
           .status(400)
-          .json({ errors: [{ msg: 'User already exists' }] });
+          .json({ errors: [{ msg: "User already exists" }] });
       }
       const avatar = gravatar.url(email, {
-        s: '200',
-        r: 'pg',
-        d: 'mm'
+        s: "200",
+        r: "pg",
+        d: "mm"
       });
       user = new User({
         name,
@@ -65,7 +66,7 @@ router.post(
       );
     } catch (err) {
       console.error(err.message);
-      res.status(500).send('Server error');
+      res.status(500).send("Server error");
     }
   }
 );
