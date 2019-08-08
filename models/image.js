c88bc4ce47eebddbf27277b024b5b2ee2cd30f27
Keith <k.farnan1@nuigalway.ie>
2019-08-08 14:49:50 +0100

    image properties modified and Joi validation

--- models/image.js
+++ models/image.js
@@ -1,47 +1,45 @@
+const Joi = require('joi');
 const mongoose = require('mongoose');
-// Pass a javaScript object to the schema method for how the product should look
-const imageSchema = mongoose.Schema({
-  // _id is convention
-  // ObjectId specific and unique
-  _id: mongoose.Schema.Types.ObjectId,
-  // date: {
-  //   type: Date,
-  //   default: Date.now
-  //   // required: true
-  // },
-  imageTitle: {
-    type: String,
-    required: true
-  },
-  // location: String,
-  // images: [
-  //   {
-  rawImageUrl: {
-    type: String
-    // required: true
-  }
-  //   }
-  // ]
-  /* images: [
-    {
-      image: {
-        type: Schema.Types.ObjectId,
-        ref: 'image'
-        imageTitle: String,
-  rawImageUrl: {
-    type: String
-    // required: true
-  },
-  jpegUrl: {
-    type: String
-  }
-  // webPUrl: {
-  //   type: String
-  // }
-      }
-    }
-  ] */
-});
 
-// this has a capital letter by convention
-module.exports = mongoose.model('Image', imageSchema);
+const Image = mongoose.model(
+  'Image',
+  new mongoose.Schema({
+    imageTitle: {
+      type: String,
+      required: true
+    },
+    imageUrl: {
+      type: String
+    }
+    // people: {
+    //   type: new mongoose.Schema({
+    //     names: [String]
+    //   })
+    // }
+  })
+);
+
+function validateImage(image) {
+  const schema = Joi.object().keys({
+    imageTitle: Joi.string()
+      .alphanum()
+      .min(3)
+      .max(30)
+      .required(),
+    imageUrl: Joi.string()
+      .alphanum()
+      .min(3)
+      .max(30)
+      .required()
+    // password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/),
+    // access_token: [Joi.string(), Joi.number()],
+    // birthyear: Joi.number().integer().min(1900).max(2013),
+    // email: Joi.string().email({ minDomainAtoms: 2 })
+    // })
+    // .with('username', 'birthyear').without('password', 'access_token');
+  });
+  return Joi.validate(image, schema);
+}
+
+exports.Image = Image;
+exports.validate = validateImage;
