045bc8f4e58d23623dd88331fa39443a6f94aab4
Keith <k.farnan1@nuigalway.ie>
2019-08-08 13:38:32 +0100

    set up Joi validation for person schema

--- models/person.js
+++ models/person.js
@@ -1,13 +1,28 @@
-const mongoose = require("mongoose");
-const personSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  name: {
-    type: String,
-    required: true,
-    // minlength: 3,
-    // maxlength: 20,
-    lowercase: true
-  }
-});
+const Joi = require('joi');
+const mongoose = require('mongoose');
+const Person = mongoose.model(
+  'Person',
+  new mongoose.Schema({
+    // _id: mongoose.Schema.Types.ObjectId,
+    name: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 20,
+      lowercase: true
+    }
+  })
+);
 
-module.exports = mongoose.model("Person", personSchema);
+function validatePerson(person) {
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .max(20)
+      .required()
+  };
+  return Joi.validate(person, schema);
+}
+
+exports.Person = Person;
+exports.validate = validatePerson;
