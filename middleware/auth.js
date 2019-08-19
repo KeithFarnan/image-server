e56b0729938ac58f8225b1197ce8ed5ad992d782
Keith <k.farnan1@nuigalway.ie>
2019-08-19 14:14:17 +0100

    Modified auth file

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,13 @@
 const jwt = require('jsonwebtoken');
 
-module.exports = (req, res, next) => {
+module.exports = function(req, res, next) {
+  // Get token from header
+  const token = req.headers.authorization.split(' ')[1];
+  // Check if not token
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, authorization denied' });
+  }
   try {
-    // Get token from header
-    const token = req.headers.authorization.split(' ')[1];
-    // Check if not token
-    if (!token) {
-      return res.status(401).json({ msg: 'No token, authorization denied' });
-    }
     // Verify token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded.user;
