62882eaa7b524b8421164277cf45a99cdb2da71d
k <k@ks-MacBook-Air.local>
2019-12-03 22:01:05 +0000

    modified jwt auth file

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,17 +3,15 @@ const jwt = require('jsonwebtoken');
 module.exports = function(req, res, next) {
   // Get token from header
   const token = req.headers.authorization.split(' ')[1];
-  // Check if not token
   if (!token) {
     return res.status(401).json({ msg: 'No token, authorization denied' });
   }
   try {
-    // Verify token
+    // if there is token verify token else return not valid token error
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded.user;
     next();
   } catch (err) {
-    // if there is no token retun error
     res.status(401).json({ msg: 'Token is not valid' });
   }
 };
